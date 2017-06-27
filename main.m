6fb6c67e25477e72e5f55b5072b4e70c59452abe
mdforbes500 <mforbes4@gmu.edu>
2017-06-27 11:11:20 -0400

    Creating the main script
    Firing it up

---
+++ main.m
@@ -0,0 +1,8 @@
+% Main script
+% ME 323 - Heat Transfer
+% Malcolm D. Forbes, Phiet Vo, Kristin Lewis, Vartan Tenkerian, 2017
+% For educational use only
+
+%For a description of the nodal arrangement, please see DIAGRAM.PDF in repository folder.
+
+%Modeling unique nodes
